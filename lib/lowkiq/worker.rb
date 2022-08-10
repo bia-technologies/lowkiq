5362e55ebb9361d9deaf1f9d0595fed9034c654e
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-08-10 17:07:25 +0400

    fix indent

--- lib/lowkiq/worker.rb
+++ lib/lowkiq/worker.rb
@@ -3,15 +3,15 @@ module Lowkiq
     attr_accessor :shards_count,
                   :batch_size,
                   :max_retry_count,
-                  :queue_name,
+                  :queue_name
 
-                  def self.extended(mod)
-                    super
-                    mod.shards_count = 5
-                    mod.batch_size = 1
-                    mod.max_retry_count = 25
-                    mod.queue_name = mod.name
-                  end
+    def self.extended(mod)
+      super
+      mod.shards_count = 5
+      mod.batch_size = 1
+      mod.max_retry_count = 25
+      mod.queue_name = mod.name
+    end
 
     # i.e. 15, 16, 31, 96, 271, ... seconds + a random amount of time
     def retry_in(retry_count)
